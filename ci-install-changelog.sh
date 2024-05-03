6ed7f2f7789cb5ad8aca5e02d8a946a25d1dc980
Mark Raynsford <code@io7m.com>
2024-05-03 20:13:03 +0000

    Use newer changelog version.

--- ci-install-changelog.sh
+++ ci-install-changelog.sh
@@ -28,8 +28,8 @@ if [ -f "$INSTALL_NAME" ]; then
   exit 0
 fi
 
-CHANGELOG_URL="https://repo1.maven.org/maven2/com/io7m/changelog/com.io7m.changelog.cmdline/4.1.0/com.io7m.changelog.cmdline-4.1.0-main.jar"
-CHANGELOG_SHA256_EXPECTED="2a38beaea7c63349c1243dbee52d97a1d048578d1132dd1b509e2d8d37445033"
+CHANGELOG_URL="https://repo1.maven.org/maven2/com/io7m/changelog/com.io7m.changelog.cmdline/5.0.0-beta0001/com.io7m.changelog.cmdline-5.0.0-beta0001-main.jar"
+CHANGELOG_SHA256_EXPECTED="ec485137b23324e61d04cc2b8ca805820fb805781f2ea9498383f477511f1d77"
 
 wget -O "${TEMP_NAME}" "${CHANGELOG_URL}" || fatal "Could not download changelog to ${TEMP_NAME}"
 mv "${TEMP_NAME}" "${INSTALL_NAME}" || fatal "Could not rename changelog from ${TEMP_NAME} to ${INSTALL_NAME}"
